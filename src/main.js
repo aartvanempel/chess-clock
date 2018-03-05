99bbadae71aa9a574bec38e01d0d1b8ca70d9f2f
avanempel <avanempel@mirabeau.nl>
2018-03-05 14:41:40 +0100

    prevent scrolling on mobile

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import store from './store/store'
 import App from './components/App/App.vue'
 
+document.ontouchmove = function(e){ e.preventDefault(); }
+
 new Vue({
   el: '#app',
   store,
